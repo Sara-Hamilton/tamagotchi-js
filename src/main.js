66bbc1ff01a2b3827303c725f0e9428e8854be9d
Hannah Lee and Sara Hamilton <sara@>
2018-03-19 11:33:53 -0700

    add name to main.js.

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import './styles.css';
 $(document).ready(function() {
   $('#name-form').submit(function(event) {
     event.preventDefault();
-    const name = $('#name').val
+    const name = $("#name").val();
 
     let tamagotchi = new Tamagotchi(name);
 
+    $("#pet-name").text(tamagotchi.name);
     $("#food").text(tamagotchi.food);
     $("#play").text(tamagotchi.play);
     $("#sleep").text(tamagotchi.sleep);
