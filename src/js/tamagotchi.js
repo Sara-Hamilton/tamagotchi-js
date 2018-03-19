f4f4e416531c50629736649db0cd1ebc39d48354
Hannah Lee and Sara Hamilton <hannah@>
2018-03-19 09:59:58 -0700

    install packages and create tamagotchi class.

--- src/js/tamagotchi.js
+++ src/js/tamagotchi.js
@@ -1,59 +1,72 @@
-// The HungryBear class is replaced with let bear
-// export class Tamagotchi {
-//
-  // constructor(name) {
-  //   this.name = name;
-  //   this.foodLevel = 10;
-  // }
-  //
-  // setHunger() {
-  //   setInterval(() => {
-  //     this.foodLevel--;
-  //   }, 1000);
-  // }
-  //
+export class Tamagotchi {
+
+  constructor(name) {
+    this.name = name;
+    this.hunger = 15;
+    this.happiness = 15;
+    this.sleepiness = 15;
+  }
+
+  setHunger() {
+    setInterval(() => {
+      this.hunger--;
+    }, 1000);
+  }
+
+  setHappiness() {
+    setInterval(() => {
+      this.happiness--;
+    }, 3000);
+  }
+
+  setSleepiness() {
+    setInterval(() => {
+      this.sleepiness--;
+    }, 5000);
+  }
+
   // didYouGetEaten() {
-  // if (this.foodLevel > 0) {
+  // if (this.hunger > 0) {
   //     return false;
   //   } else {
   //     return true;
   //   }
   // }
-  //
-  // feed() {
-  //   this.foodLevel = 10;
-  // }
-  //}
 
-
-  export let bear = {
-    foodLevel: 10,
-    setHunger: function() {
-      const hungerInterval = setInterval(() => {
-        this.foodLevel--;
-        if (this.didYouGetEaten() == true) {
-          clearInterval(hungerInterval);
-          return "You got eaten!";
-        }
-      }, 1000);
-    },
-    didYouGetEaten: function() {
-      if (this.foodLevel > 0) {
-        return false;
-      } else {
-        return true;
-      }
-    },
-    feed: function(amount) {
-      let that = this;
-      return function(food) {
-        that.foodLevel += amount
-        return `The bear ate the ${food}! Food level goes up ${amount}!`
-      }
+  feed() {
+      this.hunger = 10;
     }
-  };
+  }
 
-  bear.eatSmall = bear.feed(5);
+
+  // export let bear = {
+  //   foodLevel: 10,
+  //   setHunger: function() {
+  //     const hungerInterval = setInterval(() => {
+  //       this.foodLevel--;
+  //       if (this.didYouGetEaten() == true) {
+  //         clearInterval(hungerInterval);
+  //         return "You got eaten!";
+  //       }
+  //     }, 1000);
+  //   },
+  //   didYouGetEaten: function() {
+  //     if (this.foodLevel > 0) {
+  //       return false;
+  //     } else {
+  //       return true;
+  //     }
+  //   },
+  //   feed: function(amount) {
+  //     let that = this;
+  //     return function(food) {
+  //       that.foodLevel += amount
+  //       return `The bear ate the ${food}! Food level goes up ${amount}!`
+  //     }
+  //   }
+  // };
+  //
+  // bear.eatSmall = bear.feed(5);
 
   // function(food) {
   //     this.foodLevel += 5
