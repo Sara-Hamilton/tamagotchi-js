a6fbb8a0b4f6c0d312a377c0409519319d458a37
Hannah Lee and Sara Hamilton <sara@>
2018-03-20 10:35:51 -0700

    add weather api using ajax to main.js and corresponding html fields.

--- src/main.js
+++ src/main.js
@@ -9,12 +9,28 @@ $(document).ready(function() {
     $("#name-form").hide();
     $(".game").show();
 
+    $.ajax({
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=2668c07da7e23016b049c1d4656f0af8`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('.showHumidity').text(`The humidity in ${location} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+      },
+      error: function() {
+        $('.errors').text(`The weather conditions in ${location} are unknown.`)
+      }
+    });
+
     let tamagotchi = new Tamagotchi(name);
     tamagotchi.setFood();
     tamagotchi.setPlay();
     tamagotchi.setSleep();
 
     $("#pet-name").text(tamagotchi.name);
+    $("#pet-location").text(`${tamagotchi.name} lives in ${location}`);
     $("#food").text(tamagotchi.food);
     $("#play").text(tamagotchi.play);
     $("#sleep").text(tamagotchi.sleep);
